ea0d9d0e7e9919fc40e4aaf7b9efedbc3889b118
nurbekovemil <nurbekovemil@mail.ru>
2024-04-12 13:09:59 +0600

    cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ import { ValidationPipe } from "./pipes/validation.pipe";
 async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
